6e1174e9e8b045a1f80a84409781763038e11d9e
Oscar Amado <oaamados@gmail.com>
2018-04-22 20:02:37 -0500

    Inital commit

---
+++ checkout.rb
@@ -0,0 +1,22 @@
+class Checkout
+  def initialize(rules)
+    @rules = rules
+    @purchases = Hash.new
+  end
+
+  def scan(item)
+    if @purchases.key?(item)
+      @purchases[item] += 1
+    else
+      @purchases[item] = 1
+    end
+  end
+
+  def total
+    total = 0
+    @purchases.each do |item, quantity|
+      total += rules[item].calculate_price(quantity)
+    end
+    total
+  end
+end
